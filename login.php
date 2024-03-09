0fe7b755127c41c1843f88b4dcf9ef21c78028b1
manish-107 <creativecrew2676@gmail.com>
2024-03-09 06:49:55 +0530

    login form starts

--- login.php
+++ login.php
@@ -6,11 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log in Page</title>
     <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/global.css">
 </head>
 
 <body>
-    <section>
-
+    <section class="center">
+        <div>
+            <form action="controller/loginSubmit.php">
+                <label for="email">First name:</label>
+                <input type="email" id="email" name="email"><br><br>
+                <label for="password">Last name:</label>
+                <input type="password" id="password" name="password"><br><br>
+                <input type="submit" value="Submit">
+            </form>
+        </div>
     </section>
 </body>
 
