7b7bf1b002c365e8939b4e41390eddd4649d6963
manish-107 <creativecrew2676@gmail.com>
2024-03-10 16:43:20 +0530

    some css effect(login page)

---
+++ users/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+
+<body>
+    <div class="container element">
+        <form action="#">
+            <h1>Login</h1>
+            <div class="coolinput">
+                <label for="input" class="text">Name:</label>
+                <input type="text" placeholder="Enter your name..." name="input" class="input">
+            </div>
+            <div class="coolinput">
+                <label for="input" class="text">Password:</label>
+                <input type="password" placeholder="Enter your Password..." name="input" class="input">
+            </div>
+            <div class="center">
+
+                <button type="submit">Login</button>
+            </div>
+            <div class="lastText center"><span>TechQuery Hub - </span> Where Questions Find Answers</div>
+        </form>
+    </div>
+</body>
+
+</html>
