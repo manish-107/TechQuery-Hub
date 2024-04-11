41286ac010482717ca08bc824512a2f20dd61720
manish-107 <creativecrew2676@gmail.com>
2024-04-11 18:14:02 +0530

    some fix

--- controller/askQuestionBackend.php
+++ controller/askQuestionBackend.php
@@ -10,22 +10,22 @@ if (isset($_POST['q_submit'])) {
     $qtitle = $_POST['q_title'];
     $qdesc = $_POST['q_desc'];
     // Check if the 'q_pic' file was uploaded
-    // if(isset($_FILES['q_pic'])) {
-    //     // File uploaded successfully
-    //     $img_path = "uploads/" . basename($_FILES['q_pic']['name']);
-    //     if (move_uploaded_file($_FILES['q_pic']['tmp_name'], $img_path)) {
-    //         echo "File uploaded successfully.";
-    //     } else {
-    //         echo "Failed to upload file.";
-    //     }
-    // } else {
-    //     // 'q_pic' file was not uploaded
-    //     $img_path = ""; // Set default or handle accordingly
-    // }
+    if (isset($_FILES['q_pic'])) {
+        // File uploaded successfully
+        $img_path = "uploads/" . basename($_FILES['q_pic']['name']);
+        if (move_uploaded_file($_FILES['q_pic']['tmp_name'], $img_path)) {
+            echo "File uploaded successfully.";
+        } else {
+            echo "Failed to upload file.";
+        }
+    } else {
+        // 'q_pic' file was not uploaded
+        $img_path = ""; // Set default or handle accordingly
+    }
+    $stmt = $admin->cud("INSERT INTO `questions`( `questionid`, `user_id`, `title`, `description`, `qimage`) VALUES ('$q_id','$uid','$qtitle','$qdesc','$img_path')", "saved");
     $qtags = $_POST['q_tags'];
     $tagsArray = explode(',', $qtags);
 
-    // $stmt = $admin->cud("INSERT INTO `questions`( `questionid`, `user_id`, `title`, `description`, `qimage`) VALUES ('$q_id','$uid','$qtitle','$qdesc','$img_path')", "saved");
     foreach ($tagsArray as $tag) {
         $tag = trim($tag);
 
@@ -52,4 +52,3 @@ if (isset($_POST['q_submit'])) {
     echo "<script>alert('event added successfully'); window.location = '../getStarted.php' </script> ";
 
 }
-?>
