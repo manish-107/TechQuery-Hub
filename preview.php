a2860c288fd1238ea324349e8d15d947867eb307
manish-107 <creativecrew2676@gmail.com>
2024-04-12 23:43:02 +0530

    preview fetch data ✨

--- preview.php
+++ preview.php
@@ -1,7 +1,3 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -36,35 +32,31 @@
             <button class="btn">Ask question</button>
         </div>
     </div>
+    <?php
+    if (isset($_GET['question_id']) && isset($_GET['qusername'])) {
+        $question_id = $_GET['question_id'];
+        $qusername = $_GET['qusername'];
+    }
 
+    $stms = $admin->ret("SELECT * FROM `questions` WHERE `questionid`='$question_id'");
+    $qrow = $stms->fetch(PDO::FETCH_ASSOC);
+    ?>
     <!-- card -->
     <div class="card">
         <div style="display: flex; column-gap: 10px;flex-direction: column;">
-            <h4 style="padding: 0px;margin: 0px;font-weight:bolder;">How to Find Multiple Locations Simultaneously Using
-                Plus Codes in
-                Google Maps API? </h4>
+            <h4 style="padding: 0px;margin: 0px;font-weight:bolder;"><?php echo $qrow['title']; ?> </h4>
             <div
                 style="border-bottom: 1px solid rgba(165, 161, 161, 0.552);margin: 0px; display: flex;flex-direction: row; font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;">
-                <h5 style="padding-top: 20px;margin: 0px;">Asked : Today</h5>
+                <h5 style="padding-top: 20px;margin: 0px;">Asked : <?php echo $qrow['questioneddate']; ?> </h5>
                 <h5 style="padding: 20px;margin: 0px;">Answers : 1</h5>
-                <h5 style="padding-top: 20px;margin: 0px;">By : Manish</h5>
+                <h5 style="padding-top: 20px;margin: 0px;">By : <?php echo $qusername ?></h5>
             </div>
         </div>
 
         <div class="qcontent">
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero optio rem similique omnis voluptatibus
-                voluptates nobis sint sit, odio, veritatis repellendus tempora facere nulla debitis, necessitatibus
-                cupiditate impedit consequuntur. Cum Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-                deleniti, animi, minima modi quis voluptates pariatur odit culpa a fuga dolorum eius accusantium fugit
-                consectetur. Quas temporibus architecto ipsa laudantium quod deleniti, aliquid neque quisquam sed
-                voluptate maiores impedit, dolore, consequuntur adipisci! Soluta quo impedit consequuntur,
-                necessitatibus aliquid tenetur assumenda rerum sapiente illum? Maiores nesciunt culpa odit quo veritatis
-                beatae? Cumque pariatur explicabo libero voluptatibus, praesentium omnis saepe voluptates suscipit
-                delectus autem? Illum quo voluptatibus autem neque. Obcaecati accusantium veritatis soluta laboriosam
-                maxime excepturi sit ad! Porro officia libero ut at harum recusandae tenetur! Repellendus facilis
-                perferendis repellat facere hic!</p>
+            <p> <?php echo $qrow['description']; ?></p>
             <div>
-                <img src="images/Screenshot 2024-04-06 155010.png" width="400px" height="400px" alt="" srcset=""
+                <img src="controller/<?php echo $qrow['qimage'] ?>" width="400px" height="400px" alt="" srcset=""
                     class="preview-img">
             </div>
         </div>
