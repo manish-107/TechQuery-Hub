86a81a760e88dc8cf48142e3cd03ffdcb72f6078
manish-107 <creativecrew2676@gmail.com>
2024-05-10 16:02:59 +0530

    fix

--- app/Main.php
+++ app/Main.php
@@ -11,6 +11,7 @@ class Main
         try {
             // Attempt to create a PDO database connection using constants for database configuration
             $this->conn = new PDO(DB_TYPE . ":host=" . DB_SERVER . ";dbname=" . DB_DATABASE, DB_USER, DB_PASSWORD);
+
             // Set PDO error mode to exception for better error handling
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
