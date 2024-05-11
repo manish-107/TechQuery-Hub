7fa6ba0ad8ffe21d32aabd29e0195e338d7fbdad
manish-107 <creativecrew2676@gmail.com>
2024-05-11 15:44:03 +0530

    triggers

--- query.sql
+++ query.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (userid INT PRIMARY KEY AUTO_INCREMENT,username VARCHAR(20) N
 
 INSERT INTO users (username, password, email) VALUES('john_doe','password','john@example.com');
 
-/* question table */
+/* questions table */
 CREATE TABLE questions(questionid INT PRIMARY KEY AUTO_INCREMENT,userid INT NOT NULL,title VARCHAR(255) NOT NULL, qdesc TEXT NOT NULL,  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,FOREIGN KEY (userid) REFERENCES users(userid));
 
 INSERT INTO questions (userid, title, qdesc)VALUES (1, 'How to create a table in SQL?', 'I want to learn how to create a table in SQL.');
@@ -15,14 +15,14 @@ INSERT INTO questions (userid, title, qdesc)VALUES (1, 'How to create a table in
 /* create answers table */
 CREATE TABLE answers(answerid INT PRIMARY KEY AUTO_INCREMENT,questionid INT NOT NULL,userid INT NOT NULL,ansdesc text NOT NULL ,creationdate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,FOREIGN KEY(userid) REFERENCES users(userid),FOREIGN KEY(questionid) REFERENCES questions(questionid));
 
-INSERT INTO answers (answerid,questionid, userid, ansdesc) VALUES(1, 1, 1, 'This is the first answer to question 1.');
+INSERT INTO answers (answerid,questionid, userid, ansdesc) VALUES(1, 1, 1, 'This is the first answer to questions 1.');
 
 /* Tags table */
 CREATE TABLE tags(tagid INT PRIMARY KEY AUTO_INCREMENT,tagname varchar(15) NOT NULL);
 
 INSERT INTO Tags(TagName)VALUES('sql');
 
-/* question tag */
+/* questions tag */
 CREATE TABLE questiontags(questionid INT,tagid INT,FOREIGN KEY(questionid) REFERENCES questions(questionid),FOREIGN KEY(tagid)REFERENCES tags(tagid),PRIMARY KEY(questionID,tagid));
 
 INSERT INTO questiontags (questionid, tagid) VALUES(1, 2);
@@ -30,7 +30,7 @@ INSERT INTO questiontags (questionid, tagid) VALUES(1, 2);
 /*Answer comments */
 CREATE TABLE answercomment(acommid INT PRIMARY KEY AUTO_INCREMENT,userid INT,answerid INT,acdesc TEXT NOT NULL,FOREIGN KEY(answerid)REFERENCES answers(answerid),FOREIGN KEY(userid)REFERENCES users(userid));
 
-INSERT INTO answercomment(answerid,userid,acdesc)VALUES(1,3,'you should ask question in understanding way');
+INSERT INTO answercomment(answerid,userid,acdesc)VALUES(1,3,'you should ask questions in understanding way');
 
 /* Votes */
 CREATE TABLE votes (voteid INT PRIMARY KEY AUTO_INCREMENT,userid INT,questionid INT,votetype ENUM('like', 'dislike') NOT NULL,FOREIGN KEY (userid) REFERENCES users(userid),FOREIGN KEY (questionid) REFERENCES questions(questionid));
@@ -39,69 +39,69 @@ INSERT INTO votes (userid, questionid, votetype) VALUES (2, 1, 'like');
 
 
 
-SELECT * FROM Users;
+SELECT * FROM users;
 
-SELECT * FROM Questions;
+SELECT * FROM questions;
 
-SELECT * FROM Answers;
+SELECT * FROM answers;
 
 SELECT * FROM Tags;
 
-/*1)to retrive question and user*/
-SELECT Questions.QuestionID, Questions.UserID, Users.Username, Questions.Title, Questions.Body, Questions.CreationDate FROM Questions JOIN Users ON Questions.UserID = Users.UserID;
+    /*1)to retrive questions and user*/
+    SELECT questions.questionid, questions.userid, users.username, questions.title, questions.qdesc, questions.qcreationdate FROM questions JOIN users ON questions.userid = users.userid;
 
 
-2)//to retrive perticular question using condition//
-SELECT Questions.QuestionID, Questions.UserID, Users.Username, Questions.Title, Questions.Body, Questions.CreationDate FROM Questions JOIN Users ON Questions.UserID = Users.UserID  WHERE Questions.UserID = 4;
+2)//to retrive perticular questions using condition//
+SELECT questions.questionid, questions.userid, users.username, questions.title, questions.qdesc, questions.qcreationdate FROM questions JOIN users ON questions.userid = users.userid  WHERE questions.userid = 4;
 
 
 3)//to retrive answer and user//
-SELECT Answers.AnswerID, Answers.UserID, Users.Username, Answers.Body, Answers.CreationDate FROM Answers JOIN Users ON Answers.UserID = Users.UserID WHERE Answers.QuestionID =4;
+SELECT answers.AnswerID, answers.userid, users.username, answers.Body, answers.CreationDate FROM answers JOIN users ON answers.userid = users.userid WHERE answers.questionid =4;
 
 
-4)//to retrive answers of a question//
-SELECT Answers.QuestionID,Questions.Body AS Question,Answers.Body As Answer FROM Answers JOIN Questions ON Answers.QuestionID=Questions.QuestionID;
+4)//to retrive answers of a questions//
+SELECT answers.questionid,questions.qdesc AS Question,answers.Body As Answer FROM answers JOIN questions ON answers.questionid=questions.questionid;
 
-5)//to retrive perticular Questions answer//
-SELECT Answers.QuestionID,Questions.Body AS Question,Answers.Body AS Answers FROM Answers JOIN Questions ON Answers.QuestionID=Questions.QuestionID  WHERE  Answers.QuestionID= 4;
+5)//to retrive perticular questions answer//
+SELECT answers.questionid,questions.qdesc AS Question,answers.Body AS answers FROM answers JOIN questions ON answers.questionid=questions.questionid  WHERE  answers.questionid= 4;
 
-6)//retriving  tagname related question an answer// 
-SELECT T.TagName,Q.Body AS QuestionBody, A.Body AS AnswerBody FROM Tags T JOIN QuestionTags QT ON T.TagID = QT.TagID JOIN Questions Q ON QT.QuestionID = Q.QuestionID JOIN Answers A ON Q.QuestionID = A.QuestionID;
+6)//retriving  tagname related questions an answer// 
+SELECT T.TagName,Q.Body AS QuestionBody, A.Body AS AnswerBody FROM Tags T JOIN QuestionTags QT ON T.TagID = QT.TagID JOIN questions Q ON QT.questionid = Q.questionid JOIN answers A ON Q.questionid = A.questionid;
 
-7)//select question and answer using tags //
-SELECT Q.Body AS QuestionBody,A.Body AS AnswerBody FROM Questions Q JOIN QuestionTags QT ON Q.QuestionID = QT.QuestionID JOIN Answers A ON Q.QuestionID = A.QuestionID JOIN Tags T ON T.tagID = QT.TagID  WHERE T.TagName ="create table";
+7)//select questions and answer using tags //
+SELECT Q.Body AS QuestionBody,A.Body AS AnswerBody FROM questions Q JOIN QuestionTags QT ON Q.questionid = QT.questionid JOIN answers A ON Q.questionid = A.questionid JOIN Tags T ON T.tagID = QT.TagID  WHERE T.TagName ="create table";
 
 8)//retriving answer and comments//
-SELECT Answers.Body As Answer,answercomment.Body As Comments FROM answercomment JOIN Answers ON answercomment.AnswerID=Answers.AnswerID;
+SELECT answers.Body As Answer,answercomment.Body As Comments FROM answercomment JOIN answers ON answercomment.AnswerID=answers.AnswerID;
 
 9)//retriving answer and comments using perticular condition//
-SELECT Answers.AnswerID,Answers.Body As Answer,answercomment.Body As Comments FROM answercomment JOIN Answers ON answercomment.AnswerID=Answers.AnswerID WHERE Answers.AnswerID=4;
+SELECT answers.AnswerID,answers.Body As Answer,answercomment.Body As Comments FROM answercomment JOIN answers ON answercomment.AnswerID=answers.AnswerID WHERE answers.AnswerID=4;
 
 9)s//...//
-SELECT Answers.AnswerID,Answers.Body As Answer,answercomment.Body As Comments FROM answercomment JOIN Answers ON answercomment.AnswerID=Answers.AnswerID WHERE Answers.Body LIKE '%create table%';
+SELECT answers.AnswerID,answers.Body As Answer,answercomment.Body As Comments FROM answercomment JOIN answers ON answercomment.AnswerID=answers.AnswerID WHERE answers.Body LIKE '%create table%';
 
 
 10)//counting questions in table//
-SELECT COUNT(*) FROM Questions;
+SELECT COUNT(*) FROM questions;
 
-11)//counting question in table using condition//
-SELECT COUNT(*) FROM Questions WHERE title LIKE '%table%';
+11)//counting questions in table using condition//
+SELECT COUNT(*) FROM questions WHERE title LIKE '%table%';
 
 12)//counting likes for perticular answer//
 SELECT COUNT(*) AS NumberOfLikes FROM Votes WHERE AnswerID = 123 AND VoteType = 'like';
 
 12)//......//
-SELECT Answers.Body As Answer,COUNT(*) AS NumberOfLikes FROM votes JOIN Answers ON votes.AnswerID=Answers.AnswerID WHERE votes.AnswerID = 2 AND VoteType = 'like';
+SELECT answers.Body As Answer,COUNT(*) AS NumberOfLikes FROM votes JOIN answers ON votes.AnswerID=answers.AnswerID WHERE votes.AnswerID = 2 AND VoteType = 'like';
 
 13)//counting dislikes for perticular Question//
 SELECT COUNT(*) AS NumberOfdisLikes FROM Votes WHERE AnswerID = 3 AND VoteType = 'dislike';
 
-14)//counting number likes for perticular question//
-SELECT COUNT(*) AS NumberOfLikes FROM Votes WHERE QuestionID = 123 AND VoteType = 'like';
+14)//counting number likes for perticular questions//
+SELECT COUNT(*) AS NumberOfLikes FROM Votes WHERE questionid = 123 AND VoteType = 'like';
 
-15)//counting number of dislikes for perticular question//
-SELECT COUNT(*) AS NumberOfdisLikes FROM Votes WHERE QuestionID = 123 AND VoteType = 'dislike';
+15)//counting number of dislikes for perticular questions//
+SELECT COUNT(*) AS NumberOfdisLikes FROM Votes WHERE questionid = 123 AND VoteType = 'dislike';
 
-SELECT Users.Username, Questions.Title, Answers.Body FROM Answers JOIN Users ON Answers.UserID = Users.UserID JOIN Questions ON Answers.QuestionID = Questions.QuestionID;
+SELECT users.username, questions.title, answers.Body FROM answers JOIN users ON answers.userid = users.userid JOIN questions ON answers.questionid = questions.questionid;
 
-SELECT Users.Username, Questions.Title, Answers.Body FROM Answers JOIN Users ON Answers.UserID = Users.UserID JOIN Questions ON Answers.QuestionID = Questions.QuestionID WHERE Answers.AnswerID=3;
+SELECT users.username, questions.title, answers.Body FROM answers JOIN users ON answers.userid = users.userid JOIN questions ON answers.questionid = questions.questionid WHERE answers.AnswerID=3;
---
+++ triggers.sql
@@ -0,0 +1,64 @@
+-- triggers
+
+--check weather email already exists in database
+DELIMITER //
+
+CREATE TRIGGER before_user_insert
+BEFORE INSERT ON users
+FOR EACH ROW
+BEGIN DECLARE email_count INT;
+    SELECT COUNT(*) INTO email_count FROM users WHERE email = NEW.email;
+    IF email_count > 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Email already exists in the database';
+    END IF;
+END;
+//
+
+DELIMITER ;
+
+
+
+--create a trigger on like
+    DELIMITER //
+
+    CREATE TRIGGER question_likes
+    BEFORE INSERT ON votes
+    FOR EACH ROW
+    BEGIN
+        DECLARE votetypes VARCHAR(10);
+        DECLARE voteids INT;
+        
+        SELECT votetype, voteid INTO votetypes, voteids FROM votes WHERE questionid = NEW.questionid AND userid = NEW.userid LIMIT 1;
+        
+        IF votetypes IS NOT NULL AND votetypes <> NEW.votetype THEN
+            UPDATE votes SET votetype = NEW.votetype WHERE voteid = voteids;
+        END IF;
+    END;
+    //
+
+    DELIMITER ;
+
+
+    DELIMITER //
+
+    CREATE TRIGGER question_likes
+    BEFORE INSERT ON votes
+    FOR EACH ROW
+    BEGIN
+        DECLARE existing_voteid INT;
+
+        SELECT voteid INTO existing_voteid
+        FROM votes
+        WHERE questionid = NEW.questionid AND userid = NEW.userid
+        LIMIT 1;
+
+        IF existing_voteid IS NOT NULL THEN
+            UPDATE votes SET votetype = NEW.votetype WHERE voteid = existing_voteid;
+        ELSE
+            INSERT INTO votes (userid, questionid, votetype) VALUES (NEW.userid, NEW.questionid, NEW.votetype);
+        END IF;
+    END;
+    //
+
+    DELIMITER ;
